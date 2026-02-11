1cf103ef5bfc5a82b658467037d5f58260cfab7d
John Gerald Hughes <john.gerald.hughes@outlook.com>
2026-02-11 13:07:42 -0500

    finalising ZSUN membership list

--- JghTable/app.js
+++ JghTable/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // open terminal in project folder then >http-server . to launch dev server
-// then open browser, the terminal displays the port that the erver is listening on, it looks something like this: http://127.0.0.1:8083/html/grid01.html
+// then open browser, the terminal displays the port that the erver is listening on, it looks something like this: http://127.0.0.1:8080/html/membership.html
 
 
 
