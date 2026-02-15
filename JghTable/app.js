39179e79e049fcb207d356fbae54bd32479efcfa
John Gerald Hughes <john.gerald.hughes@outlook.com>
2026-02-14 19:52:49 -0500

    tweaks

--- JghTable/app.js
+++ JghTable/app.js
@@ -2,8 +2,10 @@
 
 // open terminal in project folder then >http-server . to launch dev server
 // then open browser, the terminal displays the port that the erver is listening on, it looks something like this: http://127.0.0.1:8080/html/membership.html
+// then open browser, the terminal displays the port that the erver is listening on, it looks something like this: http://127.0.0.1:8080/html/olympics.html
 
 // on azure, the app is hosted at https://customerzsun.blob.core.windows.net/$web/membership.html
+// on azure, the app is hosted at https://customerzsun.blob.core.windows.net/$web/olympics.html
 
 
 
